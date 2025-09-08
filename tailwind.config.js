7caa3bf0dfe112740657267e625b6fc027ef665d
kishna ram <kishanchoudhary1998@gmail.com>
2025-09-08 22:49:36 +0530

    added changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
-module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+export const content = ['./src/**/*.{js,ts,jsx,tsx}'];
+export const theme = {
+  extend: {},
 };
+export const plugins = [];
